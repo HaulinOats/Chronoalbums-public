5c7e5c5b736229fc3b476a4ddb11ed593fc6f0fd
Brett Connolly <brett84c@gmail.com>
2016-12-02 16:24:36 -0500

    changes

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || 'localhost'
 
 var client_id = 'c328ff94e771496682b76ca33ce49c26'; // Your client id
 var client_secret = '6516cf7530b3409c85c463c19db767f4'; // Your secret
-if (process.env.OPENSHIFT_NODEJS_IP) var redirect_uri = 'http://http://nodejs-brett84c.rhcloud.com//callback'; // Your redirect uri
+if (process.env.OPENSHIFT_NODEJS_IP) var redirect_uri = 'http://nodejs-brett84c.rhcloud.com//callback'; // Your redirect uri
 else var redirect_uri = 'http://'+ server_ip_address +':'+ server_port +'/callback'; // Your redirect uri
 
 /**
